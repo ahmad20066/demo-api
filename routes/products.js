b8ac563547fb297877cbbe3f5de2dba36c22a822
unknown <ahmadafif613@gmail.com>
2022-06-30 12:21:49 +0300

    aas

--- routes/products.js
+++ routes/products.js
@@ -6,12 +6,7 @@ const isAuth = require('../middleware/is_auth');
 //GET/products/shop/shopId
 
 //POST/products/addProduct
-router.post('/addProduct',[
-    body('title').trim().isLength({min : 5}).withMessage('title should have more than 5 cahracters'),
-    body('title').trim().isLength({max : 10}).withMessage('title should have less than 10 characters'),
-    body('price').isNumeric().withMessage('Please Enter a valid number'),
-    body('imageUrl').isURL().withMessage('Please Enter a valid imageUrl'),
-],controller.addProduct);
+router.post('/addProduct',controller.addProduct);
 //Get/products
 router.get('/',controller.getProducts);
 //GET/products/productById/:productId
